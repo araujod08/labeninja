d80ac417efa1b6a67dee8aa02946dbb2b9b85588
unknown <luiangcosta>
2022-04-11 17:50:33 -0300

    Home finalizada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 // import { Serviços } from './components/PaginaServico/PaginaServico'
-import Logo from './components/img/redes-sociais/Twitter.png'
+import myImg from './components/img/Logo.png'
 
 const GlobalStyles = styled.div`
     padding: 0;
@@ -16,7 +16,7 @@ const GlobalStyles = styled.div`
 const DivContainer = styled.div`
     display: flex;
     flex-direction: row;
-    background-color:#6101bd;
+    background-color: #6101bd;
     height: 100vh;
     justify-content: space-around;
     align-items: center;
@@ -26,38 +26,56 @@ const DivEsquerda = styled.div`
     display: flex;
     flex-direction:column;
     align-items: center;
-  
+    text-align: center;
+    min-width: 960px;
+    img{
+        width: 500px;
+    }
 `
 const DivDireita = styled.div`
     display: flex;
     flex-direction:column;
     align-items: center;
-    
+    text-align: center;
+    button{
+        margin: 20px;
+        width: 250px;
+        height: 35px;
+        background-color: #250045;
+        color: white;
+        border: none;
+        transition-duration: 0.4s;
+        border-radius: 180px;
+    }
+    button:hover{
+        background-color: white;
+        color: #250045;
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)
+    }
+`
+
+const DivBotoes = styled.div`
+    padding: 50px;
 `
 
 
 export default class App extends React.Component {
     render() {
-
-
         return (
             <GlobalStyles>
                 <DivContainer>
                     <DivEsquerda>
-                    <img scr={Logo} alt="logo"></img>
-                    <h1>labeninjas</h1>
+                        <img src={myImg} alt="logo labeninjas"></img>
+                        <h1>Ninjas estão por toda parte.</h1>
                     </DivEsquerda>
                     <DivDireita>
-                    <h2>depois A GENTE PENÇA</h2>
-                    <button>
-                        sou um ninja
-                    </button>
-                    <button>
-                        preciso de um ninja
-                    </button>
+                        <h1>Se você é um deles, bem-vindo. <br></br>Se precisa de um deles, bem-vindo também.</h1>
+                        <DivBotoes>
+                            <button>Sou um ninja</button>
+                            <button>Contrate um ninja</button>
+                        </DivBotoes>
                     </DivDireita>
                     {/* <Serviços /> */}
-
                 </DivContainer>
             </GlobalStyles>
         )
