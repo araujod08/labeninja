d3f010d39d88d0d4df9f6868d61595124bd98148
Adeir Maia <adeir.maia@hotmail.com>
2022-04-11 15:51:07 -0300

    Add layoyt Serviços

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ContainerHeader } from "./Style_Header";
+
+
+export const Header = () => {
+    return (
+        <ContainerHeader>
+
+        </ContainerHeader>
+    )
+}
---
+++ src/components/PaginaServico/PaginaServico.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Header } from '../Header/Header'
+import { Footer } from '../Footer/Footer'
+import { ContainerMain } from './Style_PaginaServico'
+
+
+
+export class Serviços extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <>
+                <Header />
+                <ContainerMain>
+
+                </ContainerMain>
+                <Footer />
+            </>
+
+        )
+    }
+}
