430fc5d72216426daf525ff1a967edec27e44573
arianetier <arianedemello@outlook.com>
2022-04-14 17:17:05 -0300

    Os humilhados serão exaltados em breve

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import PaginaCadastro from './components/PaginaCadastro/PaginaCadastro'
 import { PaginaServico } from './components/PaginaServico/PaginaServico'
 import Home from './components/Home/Home'
+import CardDetalhes from './components/PaginaServico/CardDetalhes'
 
 export default class App extends React.Component {
     state = {
         telaAtual: "home",
+        serviceID: '',
     }
 
     escolheTela = () => {
@@ -13,9 +15,11 @@ export default class App extends React.Component {
             case "home":
                 return <Home irParaCadastro={this.irParaCadastro} irParaServico={this.irParaServico} />
             case "souninja":
-                return <PaginaCadastro irParaHome={this.irParaHome} irParaServico={this.irParaServico} />
+                return <PaginaCadastro irParaHome={this.irParaHome} irParaServico={this.irParaServico} irParaCadastro={this.irParaCadastro} irParaDetalhes={this.irParaDetalhes} irParaCarrinho={this.irParaCarrinho} />
             case "servico":
-                return <PaginaServico irParaHome={this.irParaHome} irParaCadastro={this.irParaCadastro} />
+                return <PaginaServico irParaHome={this.irParaHome} irParaCadastro={this.irParaCadastro} irParaDetalhes={this.irParaDetalhes} irParaCarrinho={this.irParaCarrinho} />
+            case "detalhe":
+                return <CardDetalhes irParaHome={this.irParaHome} irParaServico={this.irParaServico} irParaCadastro={this.irParaCadastro} irParaDetalhes={this.irParaDetalhes} irParaCarrinho={this.irParaCarrinho} id={this.state.serviceID} />
             default:
                 return <div>Erro! Os ninjas roubaram a pagina.</div>
         }
@@ -33,11 +37,15 @@ export default class App extends React.Component {
         this.setState({ telaAtual: "home" })
     }
 
+    irParaDetalhes = (e) => {
+        this.setState({ telaAtual: "detalhe" })
+        this.setState({ serviceID: e })
+    }
+
     render() {
         return (
             <div>
                 {this.escolheTela()}
-
             </div>
         )
     }
