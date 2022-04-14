875207b2b29d6e1f72f328ea21f2c9a07b8f868f
arianetier <arianedemello@outlook.com>
2022-04-14 17:42:33 -0300

    Página Detalhes + tudo by Edu

--- src/components/PaginaServico/CardServicos.js
+++ src/components/PaginaServico/CardServicos.js
@@ -55,13 +55,6 @@ const ContainerDiversosServicos = styled.div`
     justify-content: center;
 `
 
-const BaseUrl = 'https://labeninjas.herokuapp.com/'
-const headers = {
-    headers: {
-        Authorization: '9f938b9f-4c97-4e2f-b675-1be76ea15bff'
-    }
-}
-
 export default class CardServicos extends React.Component {
     state = {
         cart: [],
@@ -127,6 +120,8 @@ export default class CardServicos extends React.Component {
                             <span>Até {newDate}</span>
                         </DivPrazo>
                         <DivBotoes>
+                            <button onClick={() => this.props.irParaDetalhes(servicos.id)}>
+                                Detalhes</button>
                             <button onClick={() => this.updateJob(servicos.id, servicos.taken)}>Comprar</button>
                         </DivBotoes>
                     </DivContainer>
@@ -134,45 +129,10 @@ export default class CardServicos extends React.Component {
             )
         })
 
-        }).sort((produtoA, produtoB) => {
-            switch (this.props.inputOrdenacao) {
-                case 'crescente':
-                    return produtoA.price - produtoB.price
-                case 'decrescente':
-                    return produtoB.price - produtoA.price
-                case 'titulo':
-                    return produtoA.title.localeCompare(produtoB.title)
-                case 'data':
-                    return Date.parse(produtoA.dueDate) - Date.parse(produtoB.dueDate)
-            }
-        })
-
-            .map((servicos) => {
-                const newDate = servicos.dueDate.slice(0, 10).split('-').reverse().join('/')
-                return (
-                    <ContainerCardServicos key={servicos.id}>
-                        <p>{servicos.title}</p>
-                        <DivContainer>
-                            <DivValor>
-                                <p>Valor</p>
-                                <span>R${servicos.price}</span>
-                            </DivValor>
-                            <DivPrazo>
-                                <p>Prazo</p>
-                                <span>Até {newDate}</span>
-                            </DivPrazo>
-                            <button onClick={()=>this.props.irParaDetalhes (servicos.id)}>
-                                Detalhes</button>
-                            {/* <CardDetalhes irParaDetalhes={this.props.irParaDetalhes} jobsDetails={this.state.jobsDetails} /> */}
-                        </DivContainer>
-                    </ContainerCardServicos>
-                )
-            })
-
-        return (
-            <ContainerDiversosServicos>
-                {diversosServicos}
-            </ContainerDiversosServicos >
-        )
+return (
+    <ContainerDiversosServicos>
+        {diversosServicos}
+    </ContainerDiversosServicos >
+)
     }
 }
